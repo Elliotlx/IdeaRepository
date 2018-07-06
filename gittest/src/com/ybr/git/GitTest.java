4f0be38266445b8918b8ec5c913686a2191a8e94
ybr <Yangbingrun@163.com>
2018-07-06 18:28:18 +0800

    second  idea branches commit

--- gittest/src/com/ybr/git/GitTest.java
+++ gittest/src/com/ybr/git/GitTest.java
@@ -3,7 +3,8 @@ package com.ybr.git;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("gitTest测试");
-        System.out.println("second gitTest测试");
+        System.out.println("gitTest");
+        System.out.println("second gitTest");
+        System.out.println("Branches third gitTest");
     }
 }
